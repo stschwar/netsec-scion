e1ee55eeb0f0140d0d1cd3067136f56513585649
Stefan Schwarz <stefan_schwarz_de@outlook.com>
2018-02-15 00:51:33 +0100

    Supervisor config adjusted for Termux environment

--- supervisor/supervisor.sh
+++ supervisor/supervisor.sh
@@ -5,7 +5,7 @@ mkdir -p logs
 # Wrap the 'supervisorctl' command
 OPTIONS="$@"
 CONF_FILE="supervisor/supervisord.conf"
-if [ ! -e /tmp/supervisor.sock ]; then
+if [ ! -e /data/data/com.termux/files/usr/tmp/supervisor.sock ]; then
     supervisord -c $CONF_FILE
 fi
 supervisorctl -c $CONF_FILE $OPTIONS
