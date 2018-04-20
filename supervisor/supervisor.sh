f329cda4286ba98e416780a21438bafd8a8ad444
stschwar <stefan_schwarz_de@outlook.com>
2018-04-20 21:56:15 +0200

    Fixed Supervisor script to check for ports instead of socket

--- supervisor/supervisor.sh
+++ supervisor/supervisor.sh
@@ -5,7 +5,7 @@ mkdir -p logs
 # Wrap the 'supervisorctl' command
 OPTIONS="$@"
 CONF_FILE="supervisor/supervisord.conf"
-if [ ! -e /data/data/com.termux/files/usr/tmp/supervisor.sock ]; then
+if [[ $(netstat -lnt | grep 9011) = *listen* ]]; then
     supervisord -c $CONF_FILE
 fi
 supervisorctl -c $CONF_FILE $OPTIONS
